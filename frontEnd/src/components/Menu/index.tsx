fb47c7489488bd7bd39458a98f48f3a62bb09618
Shystra <jlsp.2019@hotmail.com>
2023-12-04 22:52:55 -0300

    feate and styles: configuring new funcions from suspend Menu to small screens and rigth screens

--- frontEnd/src/components/Menu/index.tsx
+++ frontEnd/src/components/Menu/index.tsx
@@ -5,11 +5,11 @@ import { IoMdClose, IoMdMenu } from "react-icons/io";
 interface MenuProps {
     menuOpen: boolean;
     setMenuOpen: (open: boolean) => void;
-
 }
 
 export function Menu({ menuOpen, setMenuOpen }: MenuProps) {
     const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 768);
+    const [initialLoad, setInitialLoad] = useState(true);
 
     useEffect(() => {
         const updateScreenSize = () => {
@@ -18,23 +18,39 @@ export function Menu({ menuOpen, setMenuOpen }: MenuProps) {
 
         window.addEventListener('resize', updateScreenSize);
 
+        // Se for uma tela maior, aplique a animação após a montagem do componente
+        if (!isSmallScreen && initialLoad) {
+            setTimeout(() => {
+                setMenuOpen(true);
+                setInitialLoad(false);
+            }, 300); // Ajuste o tempo de atraso conforme necessário
+        }
+
         return () => {
             window.removeEventListener('resize', updateScreenSize);
         };
-    }, []);
+    }, [isSmallScreen, initialLoad, setMenuOpen]);
 
     const handleMenuClick = () => {
         setMenuOpen(!menuOpen);
     };
 
     return (
-        <div className={[styles.menuContainer, styles.menuOpen].join(' ')}>
-            Menu
-            {isSmallScreen && (
-                <div onClick={handleMenuClick}>
-                    {menuOpen ? <IoMdClose className={styles.iconMenu} color="red"/> : <IoMdMenu className={styles.iconMenu} color="red" />}
+        <>
+            {isSmallScreen && !menuOpen && (
+                <div onClick={handleMenuClick} className={styles.menuIcon}>
+                    <IoMdMenu />
                 </div>
             )}
-        </div>
+            <div className={[styles.menuContainer, menuOpen ? styles.menuOpen : ''].join(' ')}>
+                {isSmallScreen && (
+                    <div onClick={handleMenuClick} className={styles.closeIcon}>
+                        <IoMdClose />
+                    </div>
+                )}
+            </div>
+        </>
     );
 }
+
+
--- frontEnd/src/pages/Home/index.tsx
+++ frontEnd/src/pages/Home/index.tsx
@@ -1,14 +1,12 @@
 import { Header } from "../../components/Header";
-import { Menu } from "../../components/Menu";
 
 
 export function Home() {
-
+    
     return (
         <div>
             <Header />
             <h1>testando Home</h1>
         </div>
-    )
-
+    );
 }
