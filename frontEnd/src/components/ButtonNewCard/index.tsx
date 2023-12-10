f8894a1e930ed6d4fce5fe15561af4d210bd31c9
Shystra <jlsp.2019@hotmail.com>
2023-12-10 10:25:48 -0300

    just finishing compilation for other time

--- frontEnd/src/components/ButtonNewCard/index.tsx
+++ frontEnd/src/components/ButtonNewCard/index.tsx
@@ -2,16 +2,20 @@ import { useState } from 'react';
 import { Modal } from '../Modal';
 import styles from './ButtonNewCard.module.css';
 
-export function ButtonNewCard (){
+export function ButtonNewCard() {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
-    return(
+    return (
         <>
             <div className={styles.buttonContainer}>
                 <button onClick={() => setIsModalOpen(true)}>+ Novo Card</button>
             </div>
             <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
                 {/* Conteúdo do modal aqui */}
+                <div>
+                    {/* Coloque aqui o conteúdo que você quer que apareça no modal */}
+                    Conteúdo do Modal
+                </div>
             </Modal>
         </>
     );
